f157812b2f2b78242b9d9ee4d12b96ed278b00e4
redge35000 <lecoz.regis.pro@gmail.com>
2024-11-27 21:51:30 +0100

    init commit

---
+++ vault/src/test/java/bzh/redge/vault/VaultApplicationTests.java
@@ -0,0 +1,13 @@
+package bzh.redge.vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
